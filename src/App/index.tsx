a99ac2a7e2438f680225367ece9a0c26e26bf7f0
M. Andres Jurado <anmijurane@gmail.com>
2024-06-27 14:08:25 -0600

    ADD: entry configuration and create files

---
+++ src/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/global.css';
+
+const App = () => (
+  <div className="container">
+    <h1>TODO APP</h1>
+    <pre>by <b>anmijurane</b></pre>
+  </div>
+)
+
+export default App;
